b8093c830fecb4564c16649b74fa08c4b0284aa8
ElisabethBrocca <elisabeth.brocca.07@gmail.com>
2025-10-19 17:40:01 +0200

    [UPD] array oggetti done

---
+++ Snack 1/main.js
@@ -0,0 +1,30 @@
+
+//Creare un array di oggetti
+
+let bici = [
+   {
+    nome: 'Tom',
+    peso: 30
+   }
+   ,{
+    nome: 'jerry',
+    peso: 39
+   }
+   ,{
+    nome: 'Pippo',
+    Peso: 32
+   }
+   ,{
+    nome: 'Scobby',
+    peso: 52
+   }
+   ,{
+    nome: 'Papera',
+    peso: 27
+   }
+   ,{
+    nome: 'Madonna',
+    peso: 40
+   }
+];
+
