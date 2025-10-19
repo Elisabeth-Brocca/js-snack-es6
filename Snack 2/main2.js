08215e1e712ad26e19dd3e44ea326501104ab680
ElisabethBrocca <elisabeth.brocca.07@gmail.com>
2025-10-19 21:39:14 +0200

    [DONE] snack-2 done

--- Snack 2/main2.js
+++ Snack 2/main2.js
@@ -20,3 +20,19 @@ const squadraCalcio = [
         falli:0
     },
 ];
+
+const falliNome = [];
+
+for (let i = 0 ; i < squadraCalcio.length; i++){
+    let message = document.getElementById("falli_ricevuti");
+    
+    squadraCalcio[i]["punti"] = Math.floor((Math.random() * 30) +1);
+    squadraCalcio[i]["falli"] = Math.floor((Math.random() * 30) +1);
+
+    let{nome, falli} = squadraCalcio[i];
+    falliNome.push({nome, falli});
+
+    message.innerHTML += `<li>Squadra: <strong>${nome}</strong> <br> Falli subiti: <strong>${falli}</strong></li>`;
+};
+
+console.log(falliNome);
