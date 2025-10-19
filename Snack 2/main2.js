a07a32b7dc8952f23b82c5ee91fde110b7d013f2
ElisabethBrocca <elisabeth.brocca.07@gmail.com>
2025-10-19 19:13:02 +0200

    [DONE] snack-2 array di squadra

---
+++ Snack 2/main2.js
@@ -0,0 +1,22 @@
+const squadraCalcio = [
+    {
+        nome: 'Milan',
+        punti: 0,
+        falli:0
+    },
+    {
+        nome: 'Intern',
+        punti: 0,
+        falli:0
+    },
+    {
+        nome: 'Lazio',
+        punti: 0,
+        falli:0
+    },
+    {
+        nome: 'Cagliari',
+        punti: 0,
+        falli:0
+    },
+];
